afd3525042509746ee0c1a37fbe304f49fa1584a
Adam Souzis <adam@souzis.com>
2022-03-03 13:25:19 -0800

    add method to guess best scalar unit

--- toscaparser/elements/scalarunit.py
+++ toscaparser/elements/scalarunit.py
@@ -43,10 +43,10 @@ class ScalarUnit(object):
         else:
             for key in self.SCALAR_UNIT_DICT.keys():
                 if key.upper() == input_unit.upper():
-                    log.warning('The unit "%(unit)s" does not follow '
-                                'scalar unit standards; using "%(key)s" '
-                                'instead.' % {'unit': input_unit,
-                                              'key': key})
+                    # log.warning('The unit "%(unit)s" does not follow '
+                    #             'scalar unit standards; using "%(key)s" '
+                    #             'instead.' % {'unit': input_unit,
+                    #                           'key': key})
                     return key
             msg = (_('The unit "%(unit)s" is not valid. Valid units are '
                      '"%(valid_units)s".') %
@@ -78,19 +78,40 @@ class ScalarUnit(object):
         regex = re.compile(r'([0-9.]+)\s*(\w+)')
         if regex.match(str(self.value)):
             result = regex.match(str(self.value)).groups()
-            converted = (float(validateutils.str_to_num(result[0]))
-                         * self.SCALAR_UNIT_DICT[result[1]]
-                         / self.SCALAR_UNIT_DICT[unit])
-            if converted - int(converted) < 0.0000000000001:
-                converted = int(converted)
-            return converted
         else:
             return None
+
+        converted = (float(validateutils.str_to_num(result[0]))
+                     * self.SCALAR_UNIT_DICT[result[1]]
+                     / self.SCALAR_UNIT_DICT[unit])
+        if converted - int(converted) < 0.0000000000001:
+            converted = int(converted)
+        return converted
+
+    @classmethod
+    def to_scalar(cls, value):
+        "Find the closest unit"
+        val = float(value)
+        smallest_unit = cls.SCALAR_UNIT_UNITARY
+        smallest_val = val / smallest_unit
+        for unit, scale in cls.SCALAR_UNIT_DICT.items():
+            if scale > val:
+                continue
+            scaled = val / scale
+            if scaled < smallest_val:
+                smallest_val = scaled
+                smallest_unit = unit
+        if smallest_val - int(smallest_val) < 0.0000000000001:
+            smallest_val = int(smallest_val)
+        if round(smallest_val, 4) > 0:
+            smallest_val = round(smallest_val, 5)
+        return str(smallest_val) + " " + smallest_unit
 
 
 class ScalarUnit_Size(ScalarUnit):
 
     SCALAR_UNIT_DEFAULT = 'B'
+    SCALAR_UNIT_UNITARY = 'B'
     SCALAR_UNIT_DICT = {'B': 1, 'kB': 1000, 'KiB': 1024, 'MB': 1000000,
                         'MiB': 1048576, 'GB': 1000000000,
                         'GiB': 1073741824, 'TB': 1000000000000,
@@ -99,7 +120,8 @@ class ScalarUnit_Size(ScalarUnit):
 
 class ScalarUnit_Time(ScalarUnit):
 
-    SCALAR_UNIT_DEFAULT = 'ms'
+    SCALAR_UNIT_DEFAULT = 's'
+    SCALAR_UNIT_UNITARY = 's'
     SCALAR_UNIT_DICT = {'d': 86400, 'h': 3600, 'm': 60, 's': 1,
                         'ms': 0.001, 'us': 0.000001, 'ns': 0.000000001}
 
@@ -107,6 +129,7 @@ class ScalarUnit_Time(ScalarUnit):
 class ScalarUnit_Frequency(ScalarUnit):
 
     SCALAR_UNIT_DEFAULT = 'GHz'
+    SCALAR_UNIT_UNITARY = 'Hz'
     SCALAR_UNIT_DICT = {'Hz': 1, 'kHz': 1000,
                         'MHz': 1000000, 'GHz': 1000000000}
 
