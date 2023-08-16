01acaf56d7964f0bc405f8daff98b309e6ed0fbf
Adam Souzis <adam@souzis.com>
2023-08-16 12:45:05 -0700

    have relationship types inherit value of valid_target_types if not set

--- toscaparser/elements/relationshiptype.py
+++ toscaparser/elements/relationshiptype.py
@@ -41,7 +41,7 @@ class RelationshipType(StatefulEntityType):
 
     @property
     def valid_target_types(self):
-        return self.entity_value(self.defs, 'valid_target_types') or []
+        return self.get_value('valid_target_types', None, True) or []
 
     def _validate_keys(self):
         for key in self.defs.keys():
